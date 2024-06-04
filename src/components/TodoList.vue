5e4cf562a960599a5bbcde3c32aab6340b883d04
PfMartin <martinpfatrisch@gmail.com>
2024-06-04 22:55:41 +0200

    Created an example todo scaffold

---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-list bordered padding v-if="props.todos.length">
+    <q-item v-for="todo in props.todos" :key="todo.id">
+      <q-item-section>
+        <span :class="todo.isCompleted ? 'completed-line' : ''">{{
+          todo.content
+        }}</span>
+      </q-item-section>
+      <q-item-section avatar>
+        <q-toggle
+          class="item-secondary"
+          icon="done"
+          v-model="todo.isCompleted"
+        ></q-toggle>
+      </q-item-section>
+    </q-item>
+    <q-separator spaced />
+  </q-list>
+</template>
+
+<script setup lang="ts">
+import { Todo } from './models';
+
+interface Props {
+  todos?: Todo[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  todos: () => [],
+});
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <todo-list :todos="todos"/>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Todo } from 'components/models';
+import TodoList from 'components/TodoList.vue'
+
+defineOptions({
+  name: 'IndexPage'
+});
+
+const todos = ref<Todo[]>([
+  {
+    id: 1,
+    content: 'ct1',
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    content: 'ct2',
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    content: 'ct3',
+    isCompleted: false,
+  },
+  {
+    id: 4,
+    content: 'ct4',
+    isCompleted: false,
+  },
+  {
+    id: 5,
+    content: 'ct5',
+    isCompleted: false,
+  }
+]);
+
+</script>
