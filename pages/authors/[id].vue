d7a4f24cb392fe62e78540a79ea8b3020e5e4e14
PfMartin <martinpfatrisch@gmail.com>
2024-06-05 22:26:33 +0200

    Added new route examples

---
+++ pages/authors/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Author {{ $route.params.id }}</h1>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  title: 'Author Detail',
+  middleware: 'auth',
+})
+</script>
---
+++ pages/authors/create.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Create new author</h1>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  title: 'Create new author',
+  middleware: 'auth'
+})
+</script>
