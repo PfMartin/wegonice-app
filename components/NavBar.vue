26ef985a42cec8577b1a5ea80e2b7f0b8087a4da
PfMartin <martinpfatrisch@gmail.com>
2024-06-15 18:22:47 +0200

    Adds responsiveness to the navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+  <v-navigation-drawer
+    :permanent="!xs"
+    :temporary="xs"
+    :rail="!xs"
+    :expand-on-hover="!xs"
+    v-model="drawer"
+  >
+    <v-list>
+      <v-list-item
+        prepend-icon="mdi-account-circle"
+        subtitle="email from store"
+        title="User name from store"
+        :to="userLink"
+      >
+      </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-home" title="Home" value="home" to="/home">
+      </v-list-item>
+      <v-list-item
+        prepend-icon="mdi-head-lightbulb"
+        title="Authors"
+        value="authors"
+        to="/authors"
+      ></v-list-item>
+      <v-list-item
+        prepend-icon="mdi-book-open"
+        title="Recipes"
+        value="recipes"
+        to="/recipes"
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useDisplay } from 'vuetify'
+
+const userId = '12345678'
+const userLink = computed(() => `/user/${userId}`)
+
+const { xs } = useDisplay()
+
+const drawer = ref(false)
+
+const toggleDrawer = () => (drawer.value = !drawer.value)
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,51 +1,12 @@
 <template>
   <v-layout>
-    <v-navigation-drawer expand-on-hover rail>
-      <v-list>
-        <v-list-item
-          prepend-icon="mdi-account-circle"
-          subtitle="email from store"
-          title="User name from store"
-          :to="userLink"
-        >
-        </v-list-item>
-      </v-list>
-
-      <v-divider></v-divider>
-
-      <v-list density="compact" nav>
-        <v-list-item
-          prepend-icon="mdi-home"
-          title="Home"
-          value="home"
-          to="/home"
-        >
-        </v-list-item>
-        <v-list-item
-          prepend-icon="mdi-head-lightbulb"
-          title="Authors"
-          value="authors"
-          to="/authors"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-book-open"
-          title="Recipes"
-          value="recipes"
-          to="/recipes"
-        ></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
+    <nav-bar />
     <v-main>
-      <slot />
+      <slot></slot>
     </v-main>
   </v-layout>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-
-const userId = '12345678'
-
-const userLink = computed(() => `/user/${userId}`)
+import NavBar from '../components/NavBar.vue'
 </script>
