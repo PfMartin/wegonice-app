28b95fb5e1127b1a3d0f28a0d4c064bb376c789c
PfMartin <martinpfatrisch@gmail.com>
2024-06-22 20:59:52 +0200

    Added template for version

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    <p>Authenticate or register first</p>
-    <slot />
-  </div>
+  <v-layout>
+    <div class="w-100 h-screen d-flex flex-column justify-space-between">
+      <v-container>
+        <p>Authenticate or register first</p>
+        <slot />
+      </v-container>
+
+      <div>
+        <bottom-bar />
+      </div>
+    </div>
+  </v-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BottomBar from '../components/BottomBar.vue'
+</script>
