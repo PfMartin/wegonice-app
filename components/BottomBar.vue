f52e6946b740bec417621475dd00a6023967d33b
PfMartin <martinpfatrisch@gmail.com>
2024-06-22 22:06:01 +0200

    Improved bottom bar content

--- components/BottomBar.vue
+++ components/BottomBar.vue
@@ -3,7 +3,7 @@
     <v-row justify="center" no-gutters>
       <v-col cols="4"></v-col>
       <v-col class="text-center" cols="4"
-        >{{ new Date().getFullYear() }} <strong>WeGoNice</strong></v-col
+        >Copyright &copy; {{ yearSpan }} <strong>WeGoNice</strong></v-col
       >
       <v-col class="text-center d-flex justify-end pr-2" cols="4">{{
         config.public.version
@@ -13,5 +13,16 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+
 const config = useRuntimeConfig()
+
+const yearSpan = computed(() => {
+  const startingYear = 2024
+  const currentYear = new Date().getFullYear()
+
+  return startingYear === currentYear
+    ? currentYear
+    : `${startingYear} - ${currentYear}`
+})
 </script>
