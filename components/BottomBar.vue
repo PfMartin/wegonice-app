7a6f21a85035516ddda35a4a87cb6f2578107809
PfMartin <martinpfatrisch@gmail.com>
2024-06-22 21:12:12 +0200

    Added version info to nuxt.config.ts

--- components/BottomBar.vue
+++ components/BottomBar.vue
@@ -5,9 +5,13 @@
       <v-col class="text-center" cols="4"
         >{{ new Date().getFullYear() }} <strong>WeGoNice</strong></v-col
       >
-      <v-col class="text-center d-flex justify-end pr-2" cols="4">v0.0.0</v-col>
+      <v-col class="text-center d-flex justify-end pr-2" cols="4">{{
+        config.public.version
+      }}</v-col>
     </v-row>
   </v-footer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const config = useRuntimeConfig()
+</script>
---
+++ set_app_version.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+
+sed -i -e 's/v0.0.0/v0.0.2/g' ./nuxt.config.ts
